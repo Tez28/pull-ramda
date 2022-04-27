365e8c961e2df1ce2268ebe353408cd2ce2f314f
Tez28 <courtezcannady@gmail.com>
2022-04-27 19:31:15 -0400

    functions to pull repos

--- app.js
+++ app.js
@@ -9,9 +9,212 @@ async function main() {
     await getEslintPulls();
     await ramdaFanPulls();
     await ramdaHighPulls();
-    await ramdaLensPulls();
     await ramdaLogicPulls();
-    await ramdaGitPulls();
-    await ramdaAmgularPulls();
+    await ramdaAngularPulls();
     await ramdaRepl();
 };
+
+async function getAllRepos() {
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "gitHub",
+            message: "App initiated?"
+        }
+    ])
+    console.log(data.gitHub)
+
+    const url = `https://api.github.com/users/ramda/repos`;
+
+    const test = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.name);
+        });
+    }
+    test();
+}
+
+
+async function getRamdaPulls() {
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "pulls",
+            message: "Too see all ramda repos press enter",
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/ramda/pulls`
+
+    const runRamda = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runRamda();
+}
+
+async function getCdnjsPulls() {
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "pulls",
+            message: "To see all cdnjs pull requests press enter",
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/cdnjs/pulls`
+
+    const runCdjns = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runCdjns();
+}
+
+async function getEslintPulls() {
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "pulls",
+            message: "To see all eslint-plugin-ramda pull requests press enter",
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/eslint-plugin-ramda/pulls`
+
+    const runEslint = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runEslint();
+}
+
+async function ramdaFanPulls() {
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "pulls",
+            message: "To see all ramda-fantasy pull requests press enter",
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/ramda-fantasy/pulls`
+
+    const runRamFan = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runRamFan();
+}
+
+async function ramdaHighPulls() {
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "pulls",
+            message: "To see all ramda-highland pull requests press enter",
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/ramda-highland/pulls`
+
+    const runRamHigh = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runRamHigh();
+}
+
+
+
+async function ramdaLogicPulls() {
+    const data = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "pulls",
+            message: "To see all ramda-logic pull requests press enter",
+            choices: ['yes', 'no']
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/ramda-logic/pulls`
+
+    const runRamHigh = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runRamHigh();
+}
+
+async function ramdaAngularPulls() {
+    const data = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "pulls",
+            message: "To see all ramdaangular pull requests press enter",
+            choices: ['yes', 'no']
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/ramda-angular/pulls`
+
+    const runAng = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runAng();
+}
+
+async function ramdaRepl() {
+    const data = await inquirer.prompt([
+        {
+            type: "confirm",
+            name: "pulls",
+            message: "To see all repl pull requests press enter",
+            choices: ['yes', 'no']
+        }
+    ])
+    console.log(data.pulls)
+
+    const url = `https://api.github.com/repos/ramda/repl/pulls`
+
+    const runRamRep = async () => {
+        const request = await axios.get(url);
+
+        request.data.forEach((item) => {
+            console.log(item.number);
+        })
+    }
+    runRamRep();
+}
+main();
